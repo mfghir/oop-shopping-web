5c6616ee98d699c085bbb95a676df1ca5907be50
mfghir <fatemeghafari77@gmail.com>
2023-11-13 01:33:28 -0800

    add Products class

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+import Products from "./models/Product.js";
 import { fetchData } from "./utils/httpReq.js";
+
+const productsNode = document.getElementById("products");
 
 async function render() {
   const productData = await fetchData();
+  const productsInstance = new Products(productsNode, productData);
+  
+  productsInstance.showProducts()
 }
 
 document.addEventListener("DOMContentLoaded", render);
